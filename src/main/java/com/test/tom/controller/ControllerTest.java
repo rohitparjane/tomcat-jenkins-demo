6ecc1f537bf8396a07ba8f1e84cdfe740e7ebda2
rohitparjane <110886486+rohitparjane@users.noreply.github.com>
2025-11-21 23:07:27 +0530

    Update ControllerTest.java

--- src/main/java/com/test/tom/controller/ControllerTest.java
+++ src/main/java/com/test/tom/controller/ControllerTest.java
@@ -8,7 +8,7 @@ public class ControllerTest {
 
     @GetMapping("/HI")
     public String test(){
-        return "Hello Motherfuckers jenkins worked 4th time";
+        return "Hello Motherfuckers jenkins worked 7th time";
     }
 
 }
